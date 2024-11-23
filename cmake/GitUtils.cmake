ee7868f66aeffe653a77569d52e74feba2687bb0
newspacer <newspacer@email.com>
2024-11-23 19:18:29 +0100

    use find_package() to find git

--- cmake/GitUtils.cmake
+++ cmake/GitUtils.cmake
@@ -1,5 +1,8 @@
 
-execute_process(COMMAND /usr/bin/git describe --tags
+find_package(Git REQUIRED)
+
+
+execute_process(COMMAND ${GIT_EXECUTABLE} describe --tags
     WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
     OUTPUT_VARIABLE GIT_TAG
     OUTPUT_STRIP_TRAILING_WHITESPACE)
