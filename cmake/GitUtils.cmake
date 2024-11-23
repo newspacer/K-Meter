3100c09912dbecce03327e7906c5ead16221468a
newspacer <newspacer@email.com>
2024-11-23 14:56:12 +0100

    get version from git tag

---
+++ cmake/GitUtils.cmake
@@ -0,0 +1,15 @@
+
+execute_process(COMMAND /usr/bin/git describe --tags
+    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+    OUTPUT_VARIABLE GIT_TAG
+    OUTPUT_STRIP_TRAILING_WHITESPACE)
+
+message(STATUS "GitUtils: found tag '${GIT_TAG}'")
+
+
+string(REGEX MATCH "([0-9]\.[0-9]\.[0-9])"
+    GIT_TAG_VERSION ${GIT_TAG})
+
+
+message(STATUS "GitUtils: found version '${GIT_TAG_VERSION}'")
+
